dcaed222dcc7cc9a3fcd5e46affd8fc374a9c031
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-11-15 14:11:18 +0900

    ✨ feat: TanStack Start for Cloudflare Workersのテンプレートを追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import ts from 'typescript-eslint';
 
 const reactFileTargets = ['**/*.{jsx,tsx}'];
 
@@ -11,6 +13,13 @@ export default defineConfig([
   {
     name: 'react-hooks/react-components',
     files: reactFileTargets,
+    languageOptions: {
+      parser: ts.parser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
     ...reactHooks.configs.flat['recommended-latest'],
   },
   eslintConfigPrettier,
